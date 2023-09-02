1eaac76d3ef44051f3a3125589e3abc9763226c8
Rico042002 <rico042002@gmail.com>
2023-09-01 19:28:20 -0500

    intial commit

---
+++ gulpfile.js
@@ -0,0 +1,57 @@
+const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
+
+const paths = {
+  styles: {
+    src: ["./src/*.css"],
+    dest: "./dist/",
+  },
+  html: {
+    src: ["./src/*.html"],
+    dest: "./dist/",
+  },
+  img: {
+    src: ["./src/img/*"],
+    dest: "./dist/img",
+  },
+};
+
+/* STYLES */
+function style() {
+  return gulp
+    .src(paths.styles.src)
+    .pipe(gulp.dest(paths.styles.dest))
+    .pipe(browserSync.stream());
+}
+
+/* HTML */
+function html() {
+  return gulp
+    .src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest))
+    .pipe(browserSync.stream());
+}
+
+/* Images */
+function img() {
+  return gulp
+    .src(paths.img.src)
+    .pipe(gulp.dest(paths.img.dest))
+    .pipe(browserSync.stream());
+}
+
+/* FUNCTIONS */
+function watch() {
+  browserSync.init({
+    server: {
+      baseDir: "./src/",
+    },
+  });
+  gulp.watch(paths.img.src, img);
+  gulp.watch(paths.styles.src, style);
+  gulp.watch(paths.html.src, html);
+}
+
+/* GULP TASKS */
+exports.default = watch;
+exports.build = gulp.series(style, html, img);
